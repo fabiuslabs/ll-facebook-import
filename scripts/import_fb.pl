10b1058b5b1f60caf8fe5d07c173eacf4f08e6f6
Mark Allen <mrallen1@yahoo.com>
2013-06-12 22:41:34 -0500

    Reformat data for better Mongo query

--- scripts/import_fb.pl
+++ scripts/import_fb.pl
@@ -10,7 +10,7 @@ use 5.018;
 use JSON;
 use Text::CSV_XS;
 use Path::Tiny;
-use Data::Printer;
+#use Data::Printer;
 
 my $iter = path("../csv")->iterator();
 
@@ -30,32 +30,59 @@ my $json;
 # map world leader information by country
 
 map {;
-    $json->{$_->{COUNTRY}}->{Leader} = $_
+    my $country = delete $_->{COUNTRY};
+    $json->{$country}->{Leader} = $_
     } @{ $data->{'WorldLeaders_Facebook.csv'} };
 
 # map U.S. information by state
 
 map {; 
-    delete $_->{ID}; $json->{'United States'}->{$_->{State}}->{'Governor'} = $_ 
+    delete $_->{ID}; 
+    my $state = delete $_->{State};
+
+    $json->{'United States'}->{$state}->{'Governor'} = $_ 
     } @{ $data->{'governors_facebook.csv'} };
 
 map {; 
     delete $_->{'Page ID'}; 
-    ($_->{District} eq "Senate") 
-        ? (push @{ $json->{'United States'}->{$_->{State}}->{Senate} }, $_)
-        : ($json->{'United States'}->{$_->{State}}->{'U.S. House'}->{$_->{District}} = $_)
+    my $state = delete $_->{State};
+    my $district = delete $_->{District};
+
+    ($district eq "Senate") 
+        ? (push @{ $json->{'United States'}->{$state}->{Senate} }, $_)
+        : (push @{ $json->{'United States'}->{$state}->{'U.S. House'} }, { 'District' => $district, %{ $_ } } )
     } @{ $data->{'113th Congress Official Pages.csv'} };
 
 # map US Federal agencies by type
 
 map {;
     delete $_->{'ID'};
-    push @{ $json->{'United States'}->{'Agencies'}->{'Federal'}->{$_->{'NOTES'}} }, $_
+    my $notes = delete $_->{'NOTES'};
+
+    push @{ $json->{'United States'}->{'Agencies'}->{$notes} }, $_
     } @{ $data->{'USFederalAgencies_Facebook.csv'} };
 
 
+
+my $us = delete $json->{'United States'};
+my $agencies = delete $us->{'Agencies'};
+my $leader = delete $us->{Leader};
+
+# us_states.json formatting
+my $ar;
+map {; push @{ $ar }, { "State" => $_, %{ $us->{$_} } } } keys %{ $us };
+
+# federal_agencies.json formatting
+my $fa;
+map {; push @{ $fa }, { "Agency Type" => $_, "Agencies" => $agencies->{$_} } }  keys %{ $agencies };
+
+# world_leaders.json formatting
 my $aref;
+$json->{'United States'}->{Leader} = $leader;
+map {; push @{ $aref }, { "Country" => $_, "Leader" => { %{ $json->{$_}->{Leader} } } } } keys %{ $json };
 
-map {; push @{ $aref }, { $_ => $json->{$_} } } keys %{ $json };
+my $json_encoder = JSON->new->pretty();
 
-say JSON->new->pretty->encode($aref);
+path("../json/world_leaders.json")->spew_utf8($json_encoder->encode($aref)) or die "Couldn't write world_leaders.json: $!\n";
+path("../json/us_states.json")->spew_utf8($json_encoder->encode($ar)) or die "Couldn't write us_states.json: $!\n";
+path("../json/us_agencies.json")->spew_utf8($json_encoder->encode($fa)) or die "Couldn't write us_agencies.json: $!\n";
